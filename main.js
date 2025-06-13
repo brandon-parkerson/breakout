f1d0f27e69e22f6501d0e3a0586bc72d91d2d595
brandon-parkerson <brandon-m-parkerson@gmail.com>
2025-06-13 17:36:13 +0300

    ball colliding with the walls

--- main.js
+++ main.js
@@ -6,21 +6,35 @@ let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
 
+const ballRadius = 10;
+
 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
   }
   
-  function draw() {
+function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
     x += dx;
     y += dy;
-  }
+    // if ball hits side
+    if (x + dx > canvas.width || x + dx < 0) {
+        dx = -dx;
+    }
+    // if da ball hits da tahp or da bahtahm 
+    if (y + dy > canvas.height || y + dy < 0) {
+        dy = -dy;
+    }
+      
+}
+
+
+  
   
   
   
